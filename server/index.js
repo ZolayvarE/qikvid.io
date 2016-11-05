8db4bd97f3a81a4718ed4400af9e0e00d14e5275
ZolayvarE <ZolayvarECU@gmail.com>
2016-11-04 19:10:32 -0700

    Connect to env variable port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var server = require('./config/socket-config.js');
 
 var port = process.env.PORT || 8443;
 
-server.listen(8443, function () {
+server.listen(port, function () {
   console.log('Server now listening on port:', port);
 });
 
